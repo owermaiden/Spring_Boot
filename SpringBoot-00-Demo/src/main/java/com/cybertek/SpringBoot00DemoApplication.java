1299378f0f847cc8ba0823b936dcacd1bbc0d5ed
owermaiden <ower.maiden@cybertekschool.com>
2021-11-05 22:15:16 +0300

    SpringBoot

--- SpringBoot-00-Demo/src/main/java/com/cybertek/SpringBoot00DemoApplication.java
+++ SpringBoot-00-Demo/src/main/java/com/cybertek/SpringBoot00DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot01MvcApplication {
+public class SpringBoot00DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot01MvcApplication.class, args);
+        SpringApplication.run(SpringBoot00DemoApplication.class, args);
     }
 
 }
