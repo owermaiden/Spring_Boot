5be99902e35b717a06acc9855edb452e5fd0bf95
owermaiden <ower.maiden@cybertekschool.com>
2021-11-08 13:24:58 +0300

    SpringBoot MVC Form

---
+++ Spring_Boot-03-MVC-Form/src/main/java/com/cybertek/SpringBoot03MvcFormApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03MvcFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot03MvcFormApplication.class, args);
+    }
+
+}
---
+++ Spring_Boot-03-MVC-Form/src/test/java/com/cybertek/SpringBoot03MvcFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03MvcFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
