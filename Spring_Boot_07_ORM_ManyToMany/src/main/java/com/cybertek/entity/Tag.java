fdada485d53fd7ccca832ea465d0c0cc8de890be
owermaiden <ower.maiden@cybertekschool.com>
2021-11-23 19:11:30 +0300

    Many To Many

---
+++ Spring_Boot_07_ORM_ManyToMany/src/main/java/com/cybertek/entity/Tag.java
@@ -0,0 +1,27 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}
---
+++ Spring_Boot_07_ORM_ManyToMany/src/test/java/com/cybertek/orm/SpringBoot07OrmManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertek.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot07OrmManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
