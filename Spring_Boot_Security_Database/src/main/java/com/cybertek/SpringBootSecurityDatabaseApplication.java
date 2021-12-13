24557a0108a8fc2f135e9dcb7f80c245d27c3ebb
owermaiden <ower.maiden@cybertekschool.com>
2021-12-13 15:13:42 +0300

    database auth

---
+++ Spring_Boot_Security_Database/src/main/java/com/cybertek/SpringBootSecurityDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityDatabaseApplication.class, args);
+    }
+
+}
