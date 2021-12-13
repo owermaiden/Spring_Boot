afaed2d31efa8765bc405d06e5bb784bc22cb747
owermaiden <ower.maiden@cybertekschool.com>
2021-12-13 10:56:15 +0300

    Security AutRole

---
+++ Spring_Boot_11_Security_AutRoleBase/src/test/java/com/cybertek/SpringBoot11SecurityAutRoleBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot11SecurityAutRoleBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
