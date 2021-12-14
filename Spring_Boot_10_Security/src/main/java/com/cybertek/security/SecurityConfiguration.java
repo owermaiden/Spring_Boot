32dc7f2adab195ec8e5bf087b2043f89b47362a8
owermaiden <ower.maiden@cybertekschool.com>
2021-12-14 10:24:41 +0300

    security done

---
+++ Spring_Boot_10_Security/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.cybertek.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() //request should be authorized
+//                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/management/**").hasAnyAuthority("ADMIN","MANAGER")
+                .and() // end of authorize request
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and() // end of login
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout=true")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("cybertekSecret")
+                .userDetailsService(userPrincipalDetailsService);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
---
+++ Spring_Boot_10_Security/src/main/java/com/cybertek/security/UserPrincipalDetailsService.java
@@ -0,0 +1,31 @@
+package com.cybertek.security;
+
+import com.cybertek.entity.User;
+import com.cybertek.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(s);
+
+        if(user==null){
+            throw new UsernameNotFoundException("User does not exists");
+        }
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
