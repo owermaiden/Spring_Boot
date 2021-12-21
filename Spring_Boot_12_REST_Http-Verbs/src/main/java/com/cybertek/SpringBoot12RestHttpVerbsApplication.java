2dc88dbdb38405f057863bfc1bd9cfe9bcf720ad
owermaiden <ower.maiden@cybertekschool.com>
2021-12-21 10:05:09 +0300

    @RestController

---
+++ Spring_Boot_12_REST_Http-Verbs/src/main/java/com/cybertek/SpringBoot12RestHttpVerbsApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot12RestHttpVerbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot12RestHttpVerbsApplication.class, args);
+    }
+
+}
