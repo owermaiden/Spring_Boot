9871a9f2255c5ec8cf9051b0b08a461d2a8ef06a
owermaiden <ower.maiden@cybertekschool.com>
2021-12-21 10:27:38 +0300

    ResponseEntity

---
+++ Spring_Boot_13_REST_ResponseEntity/src/main/java/com/cybertek/SpringBook13RestResponseEntityApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBook13RestResponseEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBook13RestResponseEntityApplication.class, args);
+    }
+
+}
