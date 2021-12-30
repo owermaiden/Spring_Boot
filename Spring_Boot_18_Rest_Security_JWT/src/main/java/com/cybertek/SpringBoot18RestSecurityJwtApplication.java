3940a2934a3828adb1949975b0db127c9b5acfc3
owermaiden <ower.maiden@cybertekschool.com>
2021-12-30 15:24:28 +0300

    JWT initial commit

---
+++ Spring_Boot_18_Rest_Security_JWT/src/main/java/com/cybertek/SpringBoot18RestSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot18RestSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot18RestSecurityJwtApplication.class, args);
+    }
+
+}
