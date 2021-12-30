ac5f732b5e4b56986d4bf561b305614104d11ea7
owermaiden <ower.maiden@cybertekschool.com>
2021-12-30 16:01:08 +0300

    JWT entity done

--- Spring_Boot_18_Rest_Security_JWT/src/main/java/com/cybertek/SpringBoot18RestSecurityJwtApplication.java
+++ Spring_Boot_18_Rest_Security_JWT/src/main/java/com/cybertek/SpringBoot18RestSecurityJwtApplication.java
@@ -2,12 +2,19 @@ package com.cybertek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringBoot18RestSecurityJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot18RestSecurityJwtApplication.class, args);
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ Spring_Boot_18_Rest_Security_JWT/src/main/java/com/cybertek/exeption/ServiceException.java
@@ -0,0 +1,8 @@
+package com.cybertek.exeption;
+
+public class ServiceException extends Exception{
+
+    public ServiceException(String message){
+        super(message);
+    }
+}
